20e2fd1be7801798640446d37642be85e79801b9
motalebjalal <motalebhb@gmail.com>
2024-01-24 17:24:28 +0600

    Practice Recursion

---
+++ FebonacciRecursion.java
@@ -0,0 +1,17 @@
+package practice;
+
+public class FebonacciRecursion {
+    public static void main(String[] args) {
+        int febo = feboIn(8);
+        System.out.println(febo);
+    }
+
+    private static int feboIn(int n) {
+        //base case
+        if (n <= 1)
+            return n;
+        int first = feboIn(n - 1);
+        int second = feboIn(n - 2);
+        return first + second;
+    }
+}
