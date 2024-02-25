c0e13f07a0ef67af6ec07fe15df9f185638e924b
motalebjalal <motalebhb@gmail.com>
2024-02-25 19:43:56 +0600

    View element indexes using Enhanced for loop

---
+++ LoopInGeneric.java
@@ -0,0 +1,15 @@
+package learn_generic.generic1;
+
+public class LoopInGeneric {
+    public static <T extends Number> void evenOdd(T[] numbers) {
+        int index = 0;
+        for (T number : numbers) {
+            System.out.println("Elements : " + number + " index : " + index);
+            index++;
+        }
+    }
+    public static void main(String[] args) {
+        Integer[] elements = {10, 20, 30, 40, 50, 60, 70, 71};
+        evenOdd(elements);
+    }
+}
