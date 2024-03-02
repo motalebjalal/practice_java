046d23f98751dd16bb12362e173e767ed34b9185
motalebjalal <motalebhb@gmail.com>
2024-03-02 13:17:54 +0600

    renamed Learn_HashSet.java to LearnHashSet.java

--- LearnHashSet.java
+++ LearnHashSet.java
@@ -4,7 +4,7 @@ import java.util.HashSet;
 import java.util.Scanner;
 import java.util.Set;
 
-public class Learn_HashSet {
+public class LearnHashSet {
     public static void main(String[] args) {
 
         Set<String> foods = new HashSet<>();
