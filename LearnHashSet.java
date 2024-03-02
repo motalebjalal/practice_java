fda212aee095b4259f4d15fc41cd25fe91c955c6
motalebjalal <motalebhb@gmail.com>
2024-03-02 12:51:04 +0600

    HashSet practice from collection framework

---
+++ LearnHashSet.java
@@ -0,0 +1,38 @@
+package learn_collection.learn_set;
+
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+public class LearnHashSet {
+    public static void main(String[] args) {
+
+        Set<String> foods = new HashSet<>();
+        foods.add("Milk");
+        foods.add("Honey");
+        foods.add("Egg");
+        foods.add("Egg");
+        foods.add("Banana");
+        foods.add("Pizza");
+
+        //The order of the hash set is incorrect and cannot contain duplicate elements
+
+        System.out.println("The order of the hash set is incorrect.");
+        for(String allFoods : foods){
+            System.out.println(allFoods);
+        }
+        System.out.println("\n\n");
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("Find Your Food : ");
+        String findFood = input.nextLine();
+
+
+        if (foods.contains(findFood)){
+            System.out.println(findFood + " is present in the set.");
+        }else {
+            System.out.println(findFood+" is not present in the set");
+        }
+
+    }
+}
