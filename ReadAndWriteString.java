54f82dadf60582f89bf74c3a6478d88e16a04f18
motalebjalal <motalebhb@gmail.com>
2024-04-14 09:15:50 +0600

    Practice

---
+++ ReadAndWriteString.java
@@ -0,0 +1,23 @@
+package file_handle.practice;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class ReadAndWriteString {
+    public static void main(String[] args) throws IOException {
+
+        Path path = Path.of("/home/linux/Passion/java/basic_java/src/myfiles/read.txt");
+
+       if (Files.exists(path)) {
+            System.out.println(path.getFileName());
+        }
+
+        String text = Files.readString(path);
+        System.out.println(text);
+
+        Path writePath = Path.of("/home/linux/Passion/java/basic_java/src/myfiles/write.txt");
+        Files.writeString(writePath, text);
+
+    }
+}
